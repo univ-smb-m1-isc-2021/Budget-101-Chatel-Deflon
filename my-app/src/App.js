bfb71d7645a49d36f4d3fbaae8bf3bfa490cb756
Elodie D <elodie.deflon@gmail.com>
2022-02-14 16:27:39 +0100

    Update React and add .gitignore

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,24 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>The Gunter App</h1>
+                <h2>My Budget 101</h2>
+                {/*<img src={logo} alt="Logo"/>*/}
+            </header>
+            <div>
+                <h2>Connexion :</h2>
+                <form>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <label>Pseudo : </label>
+                                </td>
+                                <td>
+                                    <input type="text" name="username" required/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><label>Mot de passe : </label></td>
+                                <td><input type="password" name="password" required/></td>
+                            </tr>
+                            <tr>
+                                <td><input name="submit" type="submit" value="Connexion"/></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </form>
+                <div>
+                    <a href={"#"}>Mot de passe oublié</a>
+                </div>
+                <div>
+                    <a href={"#"}>Créer un compte</a>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
