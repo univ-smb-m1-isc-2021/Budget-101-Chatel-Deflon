67e1577c2714790cba6e275342f5225b8dd71784
Antoine Chatel <74916420+Chatelant@users.noreply.github.com>
2022-03-28 14:31:06 +0200

    Update and rename 1-create-db-test.sql to 1-create-db-gunter-101.sql

--- postgres-init/1-create-db-gunter-101.sql
+++ postgres-init/1-create-db-gunter-101.sql
@@ -1,7 +1,7 @@
-CREATE USER test_root WITH PASSWORD 'test!' CREATEDB;
-CREATE DATABASE db_chuck
+CREATE USER gunter_root WITH PASSWORD 'gunter_pass' CREATEDB;
+CREATE DATABASE db_gunter-101
     WITH 
-    OWNER = test_root
+    OWNER = gunter_root
     ENCODING = 'UTF8'
     LC_COLLATE = 'en_US.utf8'
     LC_CTYPE = 'en_US.utf8'
