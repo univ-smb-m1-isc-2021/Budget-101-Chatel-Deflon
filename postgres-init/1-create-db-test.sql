7f7f5e1c37c7eb57d3e00ac5c3475deb933eae08
ChatelAnt <74916420+Chatelant@users.noreply.github.com>
2022-03-28 14:45:07 +0200

    Suppr postgres-init

--- postgres-init/1-create-db-test.sql
+++
@@ -1,9 +0,0 @@
-CREATE USER test_root WITH PASSWORD 'test!' CREATEDB;
-CREATE DATABASE db_chuck
-    WITH 
-    OWNER = test_root
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.utf8'
-    LC_CTYPE = 'en_US.utf8'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
