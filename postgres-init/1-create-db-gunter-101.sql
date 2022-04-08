8b3d066110c3180d7b97d4b7f55e758e8f40bf40
Antoine Chatel <chatel.ant@gmail.com>
2022-04-08 16:17:24 +0200

    Revert "Merge remote-tracking branch 'origin/docker-branch'"
    This reverts commit ce8d1100f00c73031d0b15a1d803778fe0c2f796, reversing
    changes made to 8955fca23850639b9de46e960106d95874155235.

--- postgres-init/1-create-db-gunter-101.sql
+++
@@ -1,9 +0,0 @@
-CREATE USER gunter_root WITH PASSWORD 'gunter_pass' CREATEDB;
-CREATE DATABASE db_gunter-101
-    WITH 
-    OWNER = gunter_root
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.utf8'
-    LC_CTYPE = 'en_US.utf8'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
