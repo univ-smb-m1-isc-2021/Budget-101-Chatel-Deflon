90db849e2b01565cdf51e3febd10d26a995b9774
Antoine Chatel <chatel.ant@gmail.com>
2022-04-11 09:33:44 +0200

    Ajout du port docker

--- src/main/java/budget/adapters/web/security/DevCorsConfiguration.java
+++ src/main/java/budget/adapters/web/security/DevCorsConfiguration.java
@@ -11,7 +11,7 @@ public class DevCorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200/");
+        registry.addMapping("/**").allowedOrigins("http://localhost:4200/").allowedOrigins("https://gunter-101.oups.net/");
 //                allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 //                .exposedHeaders("Authorization");;
     }
